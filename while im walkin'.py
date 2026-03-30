4e8cb2273f0099fb72fd4402a2e29d829c9bbd74
Ashmit2013 <hiranmoybanerjee1940@gmail.com>
2026-03-30 21:41:43 +0530

    Add files via upload

---
+++ while im walkin'.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a number: "))
+sum = 0
+i = 1
+while i <= x:
+    sum = sum+i
+    i = i+1
+    print("the sum is:", sum)
+
+    print("------------------------------------")
+for y in range(1,11,1):
+    print(y)
+    print("____________________________________")
+z = int(input("enter another number"))
+for a in range(0,z,1):
+    print(a)
